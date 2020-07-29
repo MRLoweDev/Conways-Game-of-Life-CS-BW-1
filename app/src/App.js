b65da8ae144cb65eabb3d8377e37b5baeb15330a
Michael Lowe <mrlowememphis@yahoo.com>
2020-07-28 19:27:29 -0500

    Game Board code created

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components'
-import Board from './components/Board'
+import Board from './components/Board.js'
 import './App.css'
 
 
